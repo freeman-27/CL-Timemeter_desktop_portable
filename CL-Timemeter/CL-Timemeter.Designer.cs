6820cb81b52c92df507cd7dac564ee009cff09d5
Евгений Попов <immortal.27@yandex.ru>
2025-03-11 16:34:56 +0300

    Добавьте файлы проекта.

---
+++ CL-Timemeter/CL-Timemeter.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace CL_Timemeter
+{
+    partial class CL_Timemeter_Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CloseButton_Custom = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.Transparent;
+            this.StartButton.Location = new System.Drawing.Point(103, 174);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 75);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = false;
+            // 
+            // StopButton
+            // 
+            this.StopButton.BackColor = System.Drawing.Color.Transparent;
+            this.StopButton.Location = new System.Drawing.Point(184, 174);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 75);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label1.Location = new System.Drawing.Point(22, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 63);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "00:00:00";
+            // 
+            // CloseButton_Custom
+            // 
+            this.CloseButton_Custom.BackColor = System.Drawing.Color.IndianRed;
+            this.CloseButton_Custom.ForeColor = System.Drawing.SystemColors.Control;
+            this.CloseButton_Custom.Location = new System.Drawing.Point(248, -4);
+            this.CloseButton_Custom.Margin = new System.Windows.Forms.Padding(0);
+            this.CloseButton_Custom.Name = "CloseButton_Custom";
+            this.CloseButton_Custom.Size = new System.Drawing.Size(40, 40);
+            this.CloseButton_Custom.TabIndex = 3;
+            this.CloseButton_Custom.Text = "X";
+            this.CloseButton_Custom.UseVisualStyleBackColor = false;
+            this.CloseButton_Custom.Click += new System.EventHandler(this.CloseButton_Custom_Click);
+            // 
+            // CL_Timemeter_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.CloseButton_Custom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CL_Timemeter_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CL-Timemeter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button CloseButton_Custom;
+    }
+}
+
---
+++ CL-Timemeter/CL-Timemeter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CL_Timemeter
+{
+    public partial class CL_Timemeter_Form : Form
+    {
+        public CL_Timemeter_Form()
+        {
+            InitializeComponent();
+        }
+
+        //System.Drawing.Color
+
+        object BGState = CL_Timemeter_Form.DefaultBackColor;
+
+
+
+        //void BGState = System.Drawing.Color.FromArgb(((int)(((byte)(01)
+
+
+
+        private void CloseButton_Custom_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
